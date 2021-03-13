2e453b2aaec095019247a6c2a39c792cf4cec7f6
Kakashi Hatake <79625533+SreekanthH711@users.noreply.github.com>
2021-03-13 13:13:37 +0530

    Delete order new directory

--- order new/src/main/java/com/infosys/infytel/friend/repository/ProductsOrderedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.infytel.friend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.infytel.friend.entity.ProductOrdered;
-
-public interface ProductsOrderedRepository extends JpaRepository<ProductOrdered, Integer> {
-
-}
