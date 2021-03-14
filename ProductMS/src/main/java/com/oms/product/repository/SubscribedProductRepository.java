5a6157d27edf3954bda29a60ef1ab7655a0ac13e
Debdut Roy <42472741+AxzelBlaze@users.noreply.github.com>
2021-03-14 11:43:30 +0530

    Previous version of ProductMS
    Check ProductMSDemo for the updated one

--- ProductMS/src/main/java/com/oms/product/repository/SubscribedProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oms.product.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oms.product.entity.SubscribedProduct;
-
-public interface SubscribedProductRepository extends JpaRepository<SubscribedProduct, Long>{
-
-}
